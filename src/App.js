10d6d3ac9fda766fa4cb15fb1cb0df3f72685f75
Thang <79919316+ThangDao0611@users.noreply.github.com>
2021-03-29 23:23:51 +0700

    login

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import {Login} from './component/login'
+import {UserInfo} from './component/user_information'
+import "antd/dist/antd.css";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Switch>
+          <Route path='/login' exact={true} component={Login}/>
+          <Route path='/user' exact={true} component={UserInfo}/>  
+        </Switch>
+      </Router>
   );
 }
 
